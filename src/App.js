7a2f0d7de1b30dd8cab6c2da3075fa9f1ed40e8f
Valentin-Holovin <valentinholovin@gmail.com>
2022-02-21 20:27:24 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import InsultPage from './pages/InsultPage/InsultPage';
+
+const App = () => {
+  return (
+    <div className="App">
+      <InsultPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/InsultPage/InsultPage.js
@@ -0,0 +1,40 @@
+import './InsultPage.css'
+import React from 'react'
+import axios from 'axios'
+
+const InsultPage = () => {
+
+  const [insult, setInsult] = React.useState([])
+
+  const url = "https://evilinsult.com/generate_insult.php?lang=en&type=json"
+
+  React.useEffect(() => {
+      axios.get(url)
+      .then((res) => {
+          setInsult(res.data.insult)
+          console.log(res.data.insult)
+      })
+  },[])
+
+  const getInsult = () => {
+    axios.get(url)
+    .then((res) => {
+        setInsult(res.data.insult)
+    })
+  }
+
+  return (
+    <div className='insult_container'>
+            <div className='insult__block'>
+                {insult}
+            </div>
+        <button className='insult__btn'
+            onClick={getInsult}
+        >
+        GENERATE INSULT
+        </button>
+    </div>
+  )
+}
+
+export default InsultPage
